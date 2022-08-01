7ebc375714d6d3b9bcccad8c0ca1b616b46b4525
cantkillemall <cantkillemall@users.noreply.github.com>
2022-08-01 16:05:57 +0200

    Update bootstrap.js

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -7,9 +7,10 @@ module.exports = ({ strapi }) => {
     if (event.action === 'beforeFindMany' || event.action === 'beforeFindOne') {
       const populate = event.params?.populate;
 
-      if (populate && populate[0] === 'deep') {
-        const depth = populate[1] ?? 5 
-        const modelObject = getFullPopulateObject(event.model.uid, depth);
+      if ((populate && populate[0] === 'deep') || (populate && populate[0] === 'deepua')) {
+        const depth = populate[1] ?? 5
+        const populateUserAdmin = (populate[0] === 'deepua' ? true : false)
+        const modelObject = getFullPopulateObject(event.model.uid, depth, populateUserAdmin);
         event.params.populate = modelObject.populate
       }
     }
